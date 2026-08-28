1775e4f9c81643287527ceba651a7008d28f755d
Victor Brito <victorbs.brand@gmail.com>
2026-08-28 15:51:14 -0300

    fix: altera mapa base para OSM com filtro dark mode para remover marca-d'água

--- v2/js/app.js
+++ v2/js/app.js
@@ -71,13 +71,11 @@ function _setMapTheme() {
     if (currentTileLayer) {
         map.removeLayer(currentTileLayer);
     }
-    const tileUrl = isDarkMode 
-        ? "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-        : "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png";
+    const tileUrl = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
     currentTileLayer = L.tileLayer(tileUrl, {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a> &copy; <a href="https://carto.com/">CARTO</a>',
-        subdomains: "abcd",
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        className: isDarkMode ? 'map-dark-tiles' : '',
         maxZoom: 19,
     }).addTo(map);
 }
